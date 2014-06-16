62c36cc42ff8fb4eb028a14b4d6a77dc92a3d9fe
Pablo--Martinez <p.martinezben@gmail.com>
2014-06-16 17:27:15 -0300

    Se separan todos las clases en sus correspondientes .h y .cpp. Además se agregan los DataTypes DataConsulta, DataConReserva, DataEmergencia, DataDiagnostico, DataTratamiento, DataHistorial, DataRep y DataUsuario

--- Code/Debug/testTratamiento.d
+++ Code/Debug/testTratamiento.d
@@ -3,7 +3,10 @@ testTratamiento.d: ../testTratamiento.cpp ../Farmacologico.h \
  ../Quirurjico.h ../Medico.h ../IObserver.h ../Rol.h ../Usuario.h \
  ../DataUsuario.h ../DataHistorial.h ../DataMedico.h ../DataConsulta.h \
  ../DataDiagnostico.h ../DataRep.h ../Consulta.h ../Diagnostico.h \
- ../Socio.h ../ManejadorSocios.h ../Notificacion.h
+ ../Socio.h ../ManejadorSocios.h ../Notificacion.h \
+ ../ControladorTratamientos.h ../ManejadorAdministradores.h \
+ ../Administrador.h ../ManejadorMedicos.h ../ControladorDiagnosticos.h \
+ ../ManejadorMedicamentos.h
 
 ../Farmacologico.h:
 
@@ -46,3 +49,15 @@ testTratamiento.d: ../testTratamiento.cpp ../Farmacologico.h \
 ../ManejadorSocios.h:
 
 ../Notificacion.h:
+
+../ControladorTratamientos.h:
+
+../ManejadorAdministradores.h:
+
+../Administrador.h:
+
+../ManejadorMedicos.h:
+
+../ControladorDiagnosticos.h:
+
+../ManejadorMedicamentos.h:
