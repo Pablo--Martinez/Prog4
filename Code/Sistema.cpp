4959c90e29b0137211ebbfa81e12fb697d4df93f
Pablo--Martinez <p.martinezben@gmail.com>
2014-06-23 21:34:00 -0300

    Se agrega el template del main principal donde estan los casos de uso para implementar

---
+++ Code/Sistema.cpp
@@ -0,0 +1,32 @@
+
+// CASOS DE USO
+void iniciarSesión(){}
+
+void cerrarSesión(){}
+
+void altaReactivacionUsuario(){}
+
+void altaRepresentacionDiagnosticos(){}
+
+void reservaConsulta(){}
+
+void registroConsulta(){}
+
+void altaDiagnosticosConsulta(){}
+
+void altaMedicamento(){}
+
+void devolucionConsulta(){}
+
+void usuariosDadosDeAlta(){}
+
+void listarRepresentacionDiagnósticos(){}
+
+void obtenerHistorialPaciente(){}
+
+void notificarMedicos(){}
+
+//MAIN PRINCIPAL
+int main(){
+	return 0;
+}
