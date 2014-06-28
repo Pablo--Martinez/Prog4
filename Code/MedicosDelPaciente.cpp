3ae65efa146351982cfc424177bf76bfbd7841d2
carlafavale <favale.carla@gmail.com>
2014-06-28 19:04:57 -0300

    reserva de consulta

--- Code/MedicosDelPaciente.cpp
+++ Code/MedicosDelPaciente.cpp
@@ -10,10 +10,7 @@ MedicosDelPaciente::~MedicosDelPaciente() {
 
 
 set<DataMedico*> MedicosDelPaciente::algoritmoDeSeleccion(){
-<<<<<<< HEAD
 	int ci_socio = 123; // se saca de la mem del sist
-=======
->>>>>>> b7e2b535a2597197a49bb25808d86baeed5210cf
 	ManejadorSocios* ms = ManejadorSocios::getInstance();
 	Socio* s = ms->find(this->ci_tratante);
 	set<DataMedico*> medicosDelPaciente = s->obtenerMedicosDelPaciente();
